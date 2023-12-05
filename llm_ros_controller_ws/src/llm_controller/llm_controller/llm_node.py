99448e78afa40abbb7bbf0b42078fe970166672a
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-12-05 09:44:45 +0000

    imports dont work

--- llm_ros_controller_ws/src/llm_controller/llm_controller/llm_node.py
+++ llm_ros_controller_ws/src/llm_controller/llm_controller/llm_node.py
@@ -1,7 +1,210 @@
-import openai
+from swarmnet import SwarmNet
+from openai import OpenAI
+from math import pi
+from threading import Lock
 
-def main():
-    print('Hi from llm_controller.')
+import rclpy
+from rcply import Node
+from geometry_msgs.msg import Twist, Vector3
+
+CMD_FORWARD = "@FORWARD"
+CMD_ROTATE_CLOCKWISE = "@CLOCKWISE"
+CMD_ROTATE_ANTICLOCKWISE = "@ANTICLOCKWISE"
+
+LINEAR_SPEED = 0.15 # m/s
+LINEAR_DISTANCE = 0.45 # m
+
+ANGULAR_SPEED = 0.3 # rad/s
+ANGUALR_DISTANCE = pi/2.0 # rad
+
+linear_rate = node.create_rate(LINEAR_DISTANCE / LINEAR_SPEED)
+angular_rate = node.create_rate(ANGUALR_DISTANCE / ANGULAR_SPEED)
+
+global_conv = []
+client: OpenAI = None
+max_stages = 10
+this_agents_turn = True
+tl = Lock()
+vp = None
+
+class VelocityPublisher(Node):
+  def __init__(self):
+    super().__init__("velocity_publisher")
+    self.publisher = self.publisher(Twist, "/cmd_vel", 10)
+    
+    cmd = global_conv[len(global_conv)-1]["content"]
+    for s in cmd.split("\n"):
+      if(s == CMD_FORWARD):
+        self.pub_forward()
+      elif(s == CMD_ROTATE_CLOCKWISE):
+        self.pub_clockwise()
+      elif(s == CMD_ROTATE_ANTICLOCKWISE):
+        self.pub_anticlockwise()
+    
+  def _publish_cmd(self, msg: Twist):
+    self.publisher.publish(msg)
+    self.get_logger("Publishing to /cmd_vel")
+  
+  def _publish_zero(self):
+    msg = Twist()
+    
+    lin_msg = Vector3()
+    lin_msg.x = 0
+    lin_msg.y = 0
+    lin_msg.z = 0
+    
+    ang_msg = Vector3()
+    ang_msg.x = 0
+    ang_msg.y = 0
+    ang_msg.z = 0
+    
+    self._publish_cmd(msg)
+    
+  def pub_forward(self):
+    msg = Twist()
+    
+    lin_msg = Vector3()
+    lin_msg.x = LINEAR_SPEED #? X, Y or Z?
+    lin_msg.y = 0
+    lin_msg.z = 0
+    
+    ang_msg = Vector3()
+    ang_msg.x = 0
+    ang_msg.y = 0
+    ang_msg.z = 0
+    
+    self._publish_cmd(msg)
+    
+    linear_rate.sleep()
+    
+    self._publish_zero()
+    
+  def _pub_rotation(self, dir: float):
+    msg = Twist()
+    
+    lin_msg = Vector3()
+    lin_msg.x = 0
+    lin_msg.y = 0
+    lin_msg.z = 0
+    
+    ang_msg = Vector3()
+    ang_msg.x = 0
+    ang_msg.y = 0
+    ang_msg.z = dir * ANGULAR_SPEED #? X Y or Z
+    
+    self._publish_cmd(msg)
+    
+    angular_rate.sleep()
+    
+    self._publish_zero()
+    
+  def pub_anticlockwise(self):
+    self._pub_rotation(-1)
+    
+  def pub_clockwise(self):
+    self._pub_rotation(1)
+    
+
+def is_my_turn():
+  tl.acquire()
+  b = this_agents_turn
+  tl.release()
+  return b
+
+def toggle_turn():
+  global this_agents_turn
+  tl.acquire()
+  this_agents_turn = not this_agents_turn
+  tl.release()
+  
+def send_req():
+  completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=global_conv,
+    # max_tokens=500
+  )
+
+  # print(completion.choices[0].message)
+  global_conv.append({"role": completion.choices[0].message.role, "content": completion.choices[0].message.content})
+  sn_ctrl.send(f"LLM {completion.choices[0].message.role} {completion.choices[0].message.content}")
+  
+def get_api_key() -> str:
+  with open("openai_key", "r") as f:
+    return f.readline().rstrip()
+  
+def toggle_role(r: str):
+  if r == "assistant":
+    return "user"
+  elif r == "user":
+    return "assistant"
+  else:
+    return ""
+  
+def plan_completed():
+  print("Plan completed:")
+  # map(lambda m : print(f"{m.role}: {m.content}"), global_conv)
+  for m in global_conv:
+    print(f"{m['role']}: {m['content']}")
+  
+def llm_recv(msg: str) -> None: 
+  m = msg.split(" ", 1) # Msg are LLM ROLE CONTENT
+  r = m[0]
+  c = m[1]
+  global_conv.append({"role": toggle_role(r), "content": c}) #! Don't think this is adding to the list
+  toggle_turn()
+  # if("@SUPERVISOR" not in c):
+  #   send_req(client)
+  # else:
+  #   plan_completed() #? This may have issues with only one agent finishing. Could just add a SN command
+
+def negotiate():
+  current_stage = 0
+  
+  if this_agents_turn:
+    global_conv.append({"role": "user", "content": "I am at D1, you are at D7. I must end at D7 and you must end at D1"})
+  
+  while(current_stage < max_stages or not global_conv[len(global_conv)-1]["content"].endswith("@SUPERVISOR")):
+    while(not is_my_turn()): # Wait to receive from the other agent
+      sleep(0.5)
+      print("waiting")
+    
+    send_req()
+    toggle_turn()
+    current_stage += 1
+    print(f"Stage {current_stage}")
+    print(global_conv);
+      
+  plan_completed()
+  current_stage = 0
+
+def main(args=None):
+  # sn_ctrl = SwarmNet({"LLM": llm_recv})
+  # sn_ctrl.start()
+  # print("Communications initialised")
+  # input("Press any key to start")
+  # client = OpenAI(api_key=get_api_key())
+  # global_conv = [
+  #   {"role": "system", "content": f"You and I are wheeled robots, and can only move forwards, backwards, and rotate clockwise or anticlockwise.\
+  #     We will negotiate with other robots to navigate a path without colliding. You should negotiate and debate the plan until all agents agree.\
+  #       Once this has been decided you should call the '@SUPERVISOR' tag at the end of your plan and print your plan in a concise numbered list using only the following command words:\
+  #         - '{CMD_FORWARD}' to move one square forwards\
+  #         - '{CMD_ROTATE_CLOCKWISE}' to rotate 90 degrees clockwise\
+  #         - '{CMD_ROTATE_ANTICLOCKWISE}' to rotate 90 degrees clockwise\
+  #         "}]
+  # negotiate()
+  # input("Press any key to finish")
+  # sn_ctrl.kill()
+  
+  global global_conv
+  
+  global_conv = [
+    {"role": "system", "content": f"@FORWARD"}]
+  
+  rclpy.init()
+  velocity_publisher = VelocityPublisher()
+  rclpy.spin(velocity_publisher)
+  velocity_publisher.destroy_node()
+  rclpy.shutdown()
 
 
 if __name__ == '__main__':
