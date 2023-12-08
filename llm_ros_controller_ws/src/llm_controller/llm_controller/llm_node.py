0d9c79d9c3687b17fb1da231dd432990f6416b92
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-12-08 16:01:22 +0000

    working

--- llm_ros_controller_ws/src/llm_controller/llm_controller/llm_node.py
+++ llm_ros_controller_ws/src/llm_controller/llm_controller/llm_node.py
@@ -21,47 +21,44 @@ ANGULAR_SPEED = 0.3 # rad/s
 ANGULAR_DISTANCE = pi/2.0 # rad
 ANGULAR_TIME = ANGULAR_DISTANCE / ANGULAR_SPEED
 
-global_conv = []
-client: OpenAI = None
-max_stages = 10
-this_agents_turn = True
-tl = Lock()
-vp = None
-
 class VelocityPublisher(Node):
   def __init__(self):
     super().__init__("velocity_publisher")
     self.publisher_ = self.create_publisher(Twist, "/cmd_vel", 10)
+    self.global_conv = []
+    self.client: OpenAI = None
+    self.max_stages = 10
+    self.this_agents_turn = True
+    self.tl = Lock()
+  
+    # self.create_plan()
     
-    self.linear_rate = self.create_rate(LINEAR_SPEED / LINEAR_DISTANCE, self.get_clock()) # Hz so inverse the fraction
-    self.angular_rate = self.create_rate(ANGULAR_SPEED / ANGULAR_DISTANCE, self.get_clock())
+    # if(len(self.global_conv) > 0):
+    #   cmd = self.global_conv[len(self.global_conv)-1]["content"]
+    #   for s in cmd.split("\n"):
+    #     if(CMD_FORWARD in s):
+    #       self.pub_forward()
+    #     elif(CMD_ROTATE_CLOCKWISE in s):
+    #       self.pub_clockwise()
+    #     elif(CMD_ROTATE_ANTICLOCKWISE in s):
+    #       self.pub_anticlockwise()
+    #     elif(CMD_SUPERVISOR in s):
+    #       pass
+    #     else:
+    #       self.get_logger().error("Unrecognised command")
     
-    if(len(global_conv) > 0):
-      cmd = global_conv[len(global_conv)-1]["content"]
-      for s in cmd.split("\n"):
-        if(CMD_FORWARD in s):
-          self.pub_forward()
-        elif(CMD_ROTATE_CLOCKWISE in s):
-          self.pub_clockwise()
-        elif(CMD_ROTATE_ANTICLOCKWISE in s):
-          self.pub_anticlockwise()
-        elif(CMD_SUPERVISOR in s):
-          pass
-        else:
-          self.get_logger().error("Unrecognised command")
-    
-    # ss = ["@FORWARD", "@CLOCKWISE", "@FORWARD", "@CLOCKWISE"]
-    # for s in ss:
-    #   if(CMD_FORWARD in s):
-    #     self.pub_forward()
-    #   elif(CMD_ROTATE_CLOCKWISE in s):
-    #     self.pub_clockwise()
-    #   elif(CMD_ROTATE_ANTICLOCKWISE in s):
-    #     self.pub_anticlockwise()
-    #   elif(CMD_SUPERVISOR in s):
-    #     pass
-    #   else:
-    #     self.get_logger().error("Unrecognised command")
+    ss = ["@FORWARD", "@ANTICLOCKWISE", "@FORWARD", "@CLOCKWISE", "@FORWARD"]
+    for s in ss:
+      if(CMD_FORWARD in s):
+        self.pub_forward()
+      elif(CMD_ROTATE_CLOCKWISE in s):
+        self.pub_clockwise()
+      elif(CMD_ROTATE_ANTICLOCKWISE in s):
+        self.pub_anticlockwise()
+      elif(CMD_SUPERVISOR in s):
+        pass
+      else:
+        self.get_logger().error("Unrecognised command")
     
     self.get_logger().info("Full plan parsed")
         
@@ -132,111 +129,107 @@ class VelocityPublisher(Node):
     
   def pub_anticlockwise(self):
     self.get_logger().info("Anticlockwise command")
-    self._pub_rotation(-1)
+    self._pub_rotation(1)
     
   def pub_clockwise(self):
     self.get_logger().info("Clockwise command")
-    self._pub_rotation(1)
+    self._pub_rotation(-1)
     
-
-def is_my_turn():
-  tl.acquire()
-  b = this_agents_turn
-  tl.release()
-  return b
-
-def toggle_turn():
-  global this_agents_turn
-  tl.acquire()
-  this_agents_turn = not this_agents_turn
-  tl.release()
-  
-def send_req():
-  completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=global_conv,
-    # max_tokens=500
-  )
-
-  # print(completion.choices[0].message)
-  global_conv.append({"role": completion.choices[0].message.role, "content": completion.choices[0].message.content})
-  sn_ctrl.send(f"LLM {completion.choices[0].message.role} {completion.choices[0].message.content}")
-  
-def get_api_key() -> str:
-  with open("openai_key", "r") as f:
-    return f.readline().rstrip()
-  
-def toggle_role(r: str):
-  if r == "assistant":
-    return "user"
-  elif r == "user":
-    return "assistant"
-  else:
-    return ""
-  
-def plan_completed():
-  print("Plan completed:")
-  for m in global_conv:
-    print(f"{m['role']}: {m['content']}")
-  
-def llm_recv(msg: str) -> None: 
-  m = msg.split(" ", 1) # Msg are LLM ROLE CONTENT
-  r = m[0]
-  c = m[1]
-  global_conv.append({"role": toggle_role(r), "content": c}) #! Don't think this is adding to the list
-  toggle_turn()
-  # if("@SUPERVISOR" not in c):
-  #   send_req(client)
-  # else:
-  #   plan_completed() #? This may have issues with only one agent finishing. Could just add a SN command
-
-def negotiate():
-  current_stage = 0
-  
-  if this_agents_turn:
-    global_conv.append({"role": "user", "content": "I am at D1, you are at D7. I must end at D7 and you must end at D1"})
-  
-  while(current_stage < max_stages or not global_conv[len(global_conv)-1]["content"].endswith("@SUPERVISOR")):
-    while(not is_my_turn()): # Wait to receive from the other agent
-      sleep(0.5)
-      print("waiting")
+  def create_plan(self):
+    self.get_logger().info("Creating plan")
+    sn_ctrl = SwarmNet({"LLM": self.llm_recv})
+    sn_ctrl.start()
+    self.get_logger().info("Communication initialised") #! Wait until another agent connects
+    self.client = OpenAI(api_key=self.get_api_key())
+    self.global_conv = [
+      {"role": "system", "content": f"You and I are wheeled robots, and can only move forwards, backwards, and rotate clockwise or anticlockwise.\
+        We will negotiate with other robots to navigate a path without colliding. You should negotiate and debate the plan until all agents agree.\
+          Once this has been decided you should call the 'f{CMD_SUPERVISOR}' tag at the end of your plan and print your plan in a concise numbered list using only the following command words:\
+            - '{CMD_FORWARD}' to move one square forwards\
+            - '{CMD_ROTATE_CLOCKWISE}' to rotate 90 degrees clockwise\
+            - '{CMD_ROTATE_ANTICLOCKWISE}' to rotate 90 degrees clockwise\
+            "}]
+    self.negotiate()
+    sn_ctrl.kill()
     
-    send_req()
-    toggle_turn()
-    current_stage += 1
-    print(f"Stage {current_stage}")
-    print(global_conv);
+  def is_my_turn(self):
+    self.tl.acquire()
+    b = self.this_agents_turn
+    self.tl.release()
+    return b
+
+  def toggle_turn(self):
+    global this_agents_turn
+    self.tl.acquire()
+    self.this_agents_turn = not this_agents_turn
+    self.tl.release()
+    
+  def send_req(self):
+    completion = self.client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      messages=self.global_conv,
+      # max_tokens=500
+    )
+
+    # print(completion.choices[0].message)
+    self.global_conv.append({"role": completion.choices[0].message.role, "content": completion.choices[0].message.content})
+    self.sn_ctrl.send(f"LLM {completion.choices[0].message.role} {completion.choices[0].message.content}")
+    
+  def get_api_key(self) -> str:
+    with open("openai_key", "r") as f:
+      return f.readline().rstrip()
+    
+  def toggle_role(self, r: str):
+    if r == "assistant":
+      return "user"
+    elif r == "user":
+      return "assistant"
+    else:
+      return ""
+    
+  def plan_completed(self):
+    print("Plan completed:")
+    for m in self.global_conv:
+      print(f"{m['role']}: {m['content']}")
+    
+  def llm_recv(self, msg: str) -> None: 
+    m = msg.split(" ", 1) # Msg are LLM ROLE CONTENT
+    r = m[0]
+    c = m[1]
+    self.global_conv.append({"role": self.toggle_role(r), "content": c}) #! Don't think this is adding to the list
+    self.toggle_turn()
+    # if("@SUPERVISOR" not in c):
+    #   send_req(client)
+    # else:
+    #   plan_completed() #? This may have issues with only one agent finishing. Could just add a SN command
+
+  def negotiate(self):
+    current_stage = 0
+    
+    if this_agents_turn:
+      self.global_conv.append({"role": "user", "content": "I am at D1, you are at D7. I must end at D7 and you must end at D1"})
+    
+    while(current_stage < self.max_stages or not self.global_conv[len(self.global_conv)-1]["content"].endswith("@SUPERVISOR")):
+      while(not self.is_my_turn()): # Wait to receive from the other agent
+        sleep(0.5)
+        print("waiting")
       
-  plan_completed()
-  current_stage = 0
+      self.send_req()
+      self.toggle_turn()
+      current_stage += 1
+      print(f"Stage {current_stage}")
+      print(self.global_conv);
+        
+    self.plan_completed()
+    current_stage = 0
+  
 
 def main(args=None):
-  global global_conv
-  global client
-  global logger
   
   rclpy.init()
   velocity_publisher = VelocityPublisher()
-  logger = velocity_publisher.get_logger()
   
   #* Move this logic into the node itself
-  
-  sn_ctrl = SwarmNet({"LLM": llm_recv})
-  sn_ctrl.start()
-  print("Communications initialised")
-  input("Press any key to start")
-  client = OpenAI(api_key=get_api_key())
-  global_conv = [
-    {"role": "system", "content": f"You and I are wheeled robots, and can only move forwards, backwards, and rotate clockwise or anticlockwise.\
-      We will negotiate with other robots to navigate a path without colliding. You should negotiate and debate the plan until all agents agree.\
-        Once this has been decided you should call the 'f{CMD_SUPERVISOR}' tag at the end of your plan and print your plan in a concise numbered list using only the following command words:\
-          - '{CMD_FORWARD}' to move one square forwards\
-          - '{CMD_ROTATE_CLOCKWISE}' to rotate 90 degrees clockwise\
-          - '{CMD_ROTATE_ANTICLOCKWISE}' to rotate 90 degrees clockwise\
-          "}]
-  negotiate()
-  input("Press any key to finish")
-  sn_ctrl.kill()
   
   # global global_conv
   
