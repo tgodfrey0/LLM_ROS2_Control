8b8ebc8eafa9c273ab7bddd73d4edfedcbbcb89d
Toby Godfrey <toby.godfrey2003@gmail.com>
2024-01-31 12:10:57 +0000

    pkg

--- llm_ros_controller_ws/src/llm_controller/launch/llm_control_alice_launch.py
+++ llm_ros_controller_ws/src/llm_controller/launch/llm_control_alice_launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='llm_control',
+            package='llm_controller',
             executable='llm_node',
             name='llm_node',
             parameters=[{"config_file": "/home/ubuntu/LLM_ROS2_Control/configs/config_alice.yaml"}],
