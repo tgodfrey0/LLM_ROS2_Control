4e96f14a3ecfd2c11eecf5ba04479dbc7d5d3cb5
Toby Godfrey <toby.godfrey2003@gmail.com>
2024-01-31 12:09:22 +0000

    ns

--- llm_ros_controller_ws/src/llm_controller/launch/llm_control_alice_launch.py
+++ llm_ros_controller_ws/src/llm_controller/launch/llm_control_alice_launch.py
@@ -5,7 +5,6 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='llm_control',
-            namespace='/alice',
             executable='llm_node',
             name='llm_node',
             parameters=[{"config_file": "/home/ubuntu/LLM_ROS2_Control/configs/config_alice.yaml"}],
