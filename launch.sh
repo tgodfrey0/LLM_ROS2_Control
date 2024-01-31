01b0dd11348b3eaedd2f177f9d2e25dc9fcd471d
Toby Godfrey <toby.godfrey2003@gmail.com>
2024-01-31 19:38:05 +0000

    launch

--- launch.sh
+++ launch.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Create the session named "llm_control" in detached mode
 tmux new-session -d -s llm_control
 tmux split-window -h
 tmux split-window -v
 
-# Create the second window named "ros2-bg"
 tmux new-window -n ros2-bg -t llm_control:2
 tmux split-window -h
 tmux split-window -v
 
-# Optional: Attach to the session if you want to start working in it immediately
 tmux attach-session -t llm_control
