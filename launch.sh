df60a830604ec9cafbd89cb679c27f12e1d6f77e
Toby Godfrey <toby.godfrey2003@gmail.com>
2024-01-31 21:34:23 +0000

    term

--- launch.sh
+++ launch.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-tmux new-session -d -s llm_control
-tmux split-window -h
-tmux split-window -v
+# Start a new Tmux session named "llm_control"
+tmux new-session -d -s llm_control -n main
 
-tmux new-window -n ros2-bg -t llm_control:2
+# Create the "main" window with three panes
 tmux split-window -h
-tmux split-window -v
+tmux split-window -v -t 1
 
+# Create the "ros2-bg" window with four panes
+tmux new-window -n ros2-bg
+tmux split-window -v
+tmux split-window -h -t 0
+tmux split-window -h -t 2
+
+# Attach to the "llm_control" session
 tmux attach-session -t llm_control
--- llm_ros_controller_ws/src/llm_controller/llm_controller/llm_node.py
+++ llm_ros_controller_ws/src/llm_controller/llm_controller/llm_node.py
@@ -228,7 +228,7 @@ class VelocityPublisher(Node):
     self.global_conv = [
         {"role": "system", "content": f"You and I are wheeled robots, and can only move forwards, backwards, and rotate clockwise or anticlockwise.\
           We will negotiate with other robots to navigate a path without colliding. You should negotiate and debate the plan until all agents agree.\
-            You cannot go outside of the grid and we cannot be in the same grid square at once. Only one of us can fit in a square at once. \ 
+            You cannot go outside of the grid and we cannot be in the same grid square at once. Only one of us can fit in a square at once.\
             Once this has been decided you should call the '\f{self.CMD_SUPERVISOR}' tag at the end of your plan and print your plan in a concise numbered list using only the following command words:\
               - '{self.CMD_FORWARD}' to move one square forwards\
               - '{self.CMD_BACKWARDS}' to move one square backwards \
