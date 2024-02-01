b3017651e91bbc91b6c689194a5da1a6a67da2d9
Toby Godfrey <toby.godfrey2003@gmail.com>
2024-02-01 11:50:43 +0000

    m

--- llm_ros_controller_ws/src/llm_controller/llm_controller/llm_node.py
+++ llm_ros_controller_ws/src/llm_controller/llm_controller/llm_node.py
@@ -72,7 +72,7 @@ class VelocityPublisher(Node):
         {"role": "system", "content": f"You and I are wheeled robots, and can only move forwards, backwards, and rotate clockwise or anticlockwise.\
           We will negotiate with other robots to navigate a path without colliding. You should negotiate and debate the plan until all agents agree.\
             You cannot go outside of the grid and we cannot be in the same grid square at once. Only one of us can fit in a square at once.\
-            Once this has been decided you should call the '\f{self.CMD_SUPERVISOR}' tag at the end of your plan and print your plan in a concise numbered list using only the following command words:\
+            Once this has been decided you should call the '\f{self.CMD_SUPERVISOR}' tag at the end of the message and print your plan in a concise numbered list using only the following command words:\
               - '{self.CMD_FORWARD}' to move one square forwards\
               - '{self.CMD_BACKWARDS}' to move one square backwards \
               - '{self.CMD_ROTATE_CLOCKWISE}' to rotate 90 degrees clockwise (and stay in the same square) \
@@ -350,7 +350,7 @@ class VelocityPublisher(Node):
     with open(path, "a", newline="") as csvfile:
       writer = csv.writer(csvfile)
       today = datetime.date.today().strftime("%Y-%m-%d")
-      writer.writerow([today, self.model, str(self.MAX_NUM_NEGOTIATION_MESSAGES), str(n_stages)])
+      writer.writerow([today, self.MODEL_NAME, str(self.MAX_NUM_NEGOTIATION_MESSAGES), str(n_stages)])
 
   def restart_recv(self, msg: Optional[str]) -> None:
     self.restart(False)
