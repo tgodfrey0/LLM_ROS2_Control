ecf730cd89fa2835ca0e638373206e921f92298f
Toby Godfrey <toby.godfrey2003@gmail.com>
2024-02-06 22:13:57 +0000

    prompt

--- llm_ros_controller_ws/src/llm_controller/llm_controller/llm_node.py
+++ llm_ros_controller_ws/src/llm_controller/llm_controller/llm_node.py
@@ -106,20 +106,36 @@ class VelocityPublisher(Node):
     #         We cannot fit past each other in the narrow corridor and we cannot leave the corridor. We cannot move past\
     #         The final plan should be a numbered list only containing these commands and we should try to complete the task as quickly as possible."}]
     
+    # self.global_conv = [
+    #     {"role": "system", "content": f"You and I are on a grid and can only move forwards, backwards, and rotate clockwise or anticlockwise.\
+    #       We will negotiate with each other to navigate a path without colliding. You should negotiate and debate the plan until all agents agree.\
+    #         We cannot go outside of the grid and we cannot be in the same grid square at once. Only one of us can fit in a square at once.\
+    #         Once this has been decided you should call the '\f{self.CMD_SUPERVISOR}' tag at the end of the message but you should agree on the plan first. And print your plan in a concise numbered list using only the following command words:\
+    #           - '{self.CMD_FORWARD}' to move one square forwards\
+    #           - '{self.CMD_BACKWARDS}' to move one square backwards \
+    #           - '{self.CMD_ROTATE_CLOCKWISE}' to rotate 90 degrees clockwise (and stay in the same square) \
+    #           - '{self.CMD_ROTATE_ANTICLOCKWISE}' to rotate 90 degrees clockwise (and stay in the same square) \
+    #           - '{self.CMD_WAIT}' to wait for the time taken to move one square \
+    #         Do not create new commands and we should both move at the same time.\
+    #         We should keep our messages as short as possible.\
+    #         We cannot fit past each other in the narrow corridor and we cannot leave the corridor. Areas A3 and C4 allow one of us to move out of the way of the other.\
+    #         The final plan should be a numbered list only containing these commands and we should try to complete the task as quickly as possible"}]
+    
     self.global_conv = [
-        {"role": "system", "content": f"You and I are on a grid and can only move forwards, backwards, and rotate clockwise or anticlockwise.\
-          We will negotiate with each other to navigate a path without colliding. You should negotiate and debate the plan until all agents agree.\
-            We cannot go outside of the grid and we cannot be in the same grid square at once. Only one of us can fit in a square at once.\
+        {"role": "system", "content": f"You are on a grid and can only move forwards, backwards, and rotate clockwise or anticlockwise.\
+          You will negotiate with another agent on the grid to navigate a path without colliding. You should negotiate and debate the plan until all agents agree.\
+            You cannot go outside of the grid and you cannot be in the same grid square as another other agent at the same time. Only one agent can fit in a square at once.\
             Once this has been decided you should call the '\f{self.CMD_SUPERVISOR}' tag at the end of the message but you should agree on the plan first. And print your plan in a concise numbered list using only the following command words:\
               - '{self.CMD_FORWARD}' to move one square forwards\
               - '{self.CMD_BACKWARDS}' to move one square backwards \
               - '{self.CMD_ROTATE_CLOCKWISE}' to rotate 90 degrees clockwise (and stay in the same square) \
               - '{self.CMD_ROTATE_ANTICLOCKWISE}' to rotate 90 degrees clockwise (and stay in the same square) \
               - '{self.CMD_WAIT}' to wait for the time taken to move one square \
-            Do not create new commands and we should both move at the same time.\
-            We should keep our messages as short as possible.\
-            We cannot fit past each other in the narrow corridor and we cannot leave the corridor. Areas A3 and C4 allow one of us to move out of the way of the other.\
-            The final plan should be a numbered list only containing these commands and we should try to complete the task as quickly as possible."}]
+            Do not create new commands and all agents start movement at the same time.\
+            The space is discrete and so you can think of it in terms of the grid.\
+            You should try to keep your messages as short as possible.\
+            You cannot fit past other agents in the narrow corridor and you cannot leave the corridor. Areas A3 and C4 allow one of us to move out of the way of the other.\
+            The final plan should be a numbered list only containing these commands and we should try to complete the task as quickly as possible. Make the plan specific for this discrete environment."}]
     
     
     if("vision" in self.MODEL_NAME):
